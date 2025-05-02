17eaf59a911ff6eab8d96ca475722113617c5751
snehamaryshaji <snehamaryshaji4@gmail.com>
2025-05-02 13:31:12 +0530

    changed

--- assessments/week03/week03/problem1.cpp
+++ assessments/week03/week03/problem1.cpp
@@ -1,7 +1,44 @@
 #include<iostream>
+#include<string>
 using namespace std;
+int pos(char str[],char str2[])
+{
+int i = 0;
+char *position = strstr(str, str2);
+if (position)
+{
+	return position - str;
+}
+else
+{
+	return -1;
+}
+}
+int pos2(char *str)
+{
+	int count = 0;
+	while (*str)
+	{
+		if (*str == 'i')
+		{
+			return count;
+		}
+		else
+		{
+			str++;
+			count++;
+		}
+	}
+}
 int main()
 {
-	int char str[]
+	char str[200];
+	cout << "enter the sentance" << endl;
+	cin.getline(str, 200);
+	char str2[20] = "are";
+	int posa=pos(str,str2);
+	int posb = pos2(str);
+		int sum = posa + posb;
+	cout << sum;
 	return 0;
 }
