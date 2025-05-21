668cc2e6fe4d51ad95a1aaf531839e24f3d7cffe
snehamaryshaji <snehamaryshaji4@gmail.com>
2025-05-21 23:17:38 +0530

    new

--- assignment/day26/day26/digitalclovk.cpp
+++ assignment/day26/day26/digitalclovk.cpp
@@ -6,20 +6,59 @@ class clock
 	int min;
 	int second;
 public:
-	void set_time();
+	clock(int h = 0, int m = 0, int s = 0)
+	{
+		if (hour >= 0 && hour <= 24)
+		{
+			hour = h;
+		}
+		else {
+			hour = 0;
+		}
+		if (min >= 0 && hour <= 60)
+		{
+			min = m;
+		}
+		else {
+			min = 0;
+		}
+		if (second >= 0 && second <= 60)
+		{
+			second = s;
+		}
+		else {
+			second= 0;
+		}
+	}
 	void tik();
+	void disply_time();
 };
-void clock:: set_time()
+void clock::tik()
 {
-	cout << "enter the hour" << endl;
-	cin >> hour;
-	cout << "enter the min" << endl;
-	cin >> min;
-	cout << "enter the sec" << endl;
-	cin >> second;
-
+	second++;
+	if (second == 60)
+	{
+		second = 0;
+		min++;
+		if (min == 60)
+		{
+			min = 0;
+			hour++;
+			if (hour == 24)
+			{
+				hour = 0;
+			}
+		}
+	}
+}
+void clock::disply_time()
+{
+	cout << hour << ":" << min << ":" << second << endl;
 }
 int main()
 {
+	clock s(20, 56, 8);
+	s.disply_time();
+	s.tik();
 	return 0;
 }
