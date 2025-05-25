17d2ac3660d78575f76b4868430367a66131abd6
snehamaryshaji <snehamaryshaji4@gmail.com>
2025-05-25 22:32:33 +0530

    new

---
+++ classwork/day29/day29/searchUpdateDlt.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+typedef struct node
+{
+	int data;
+	struct node* next;
+}NODE;
+
+NODE* createNode();
+NODE* addNodeBeg(NODE*, NODE*);
+NODE* addNodeEnd(NODE*, NODE*);
+int dispList(NODE*);
+int dispMenu();
+void freeNodes(NODE*);
+NODE* search(NODE*,int);
+//NODE* delete(NODE*, NODE*);
+
+int main()
+{
+	NODE* head = NULL;
+	int ch = 1;
+	while (ch)
+	{
+		switch (dispMenu())
+		{
+		case 1:
+
+			head = addNodeBeg(head, createNode());
+			break;
+		case 2:
+			head = addNodeEnd(head, createNode());
+			break;
+		case 3:
+			dispList(head);
+			break;
+		case 4:
+			int num;
+			cout << "enter the number" << endl;
+			cin >> num;
+			search(head,num);
+			break;
+		case 5:
+			ch = 0;
+			break;
+		default:
+			cout << "Something went wrong" << endl;
+		}
+	}
+	freeNodes(head); //*very improtant Memory Leaks
+	cout << "Exiting program...." << endl;
+	return 0;
+}
+
+
+int dispMenu()
+{
+	int ch;
+	cout << "Press,\n1. Add Node Begining\n2. Add Node End\n";
+	cout << "\t3. Display List\t4.search\n\t5. Exit..\n\tChoice: ";
+	cin >> ch;
+	return ch;
+}
+
+int dispList(NODE* head)
+{
+	cout << "\tList is " << endl;
+	cout << "\t";
+	while (head != NULL)
+	{
+		cout << head->data << " -> ";
+		head = head->next;
+	}
+	cout << "NULL" << endl;
+	return EXIT_SUCCESS;
+}
+
+NODE* addNodeEnd(NODE* head, NODE* nn)
+{
+	NODE* temp = head;
+	if (head == NULL) {
+		head = nn;
+		return head;
+	}
+	while (head->next != NULL)
+		head = head->next;
+	head->next = nn;
+	head = temp;
+	return head;
+}
+
+NODE* addNodeBeg(NODE* head, NODE* nn)
+{
+	if (head == NULL)
+	{
+		head = nn;
+		return head;
+	}
+	nn->next = head;
+	head = nn;
+	return head;
+}
+
+NODE* createNode()
+{
+	NODE* nn = (NODE*)malloc(sizeof(NODE));
+	cout << "Enter the value of node: ";
+	cin >> nn->data;
+	nn->next = NULL;
+	return nn;
+}
+
+void freeNodes(NODE* head)
+{
+	NODE* temp = head;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
+NODE* search(NODE*head,int num)
+{
+	NODE* temp = head;
+	if (head == NULL)
+	{
+		cout <<num<< " not element" << endl;
+		return head;
+	}
+	else
+	{
+		int count = 1;
+		while (temp->next != NULL)
+		{
+			if (temp->data == num)
+			{
+				cout << num << " is found at node " << count << endl;
+				return temp;
+			}
+			temp = temp->next;
+			count++;
+			
+		}
+		cout << num << " is not found " << endl;
+
+	}
+	return head;
+
+}
--- classwork/day29/day29/slst.cpp
+++ classwork/day29/day29/slst.cpp
@@ -1,106 +1,21 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
-
-class node
+class Node
 {
+private:
 	int data;
-	class node* next;
-public:
-
-node* createNode();
-node* addNodeBeg(node*, node*);
-node* addNodeEnd(node*, node*);
-int dispList(node*);
-int dispMenu();
+	Node* next;
 };
-int main()
-{
-	node* head = NULL, * nn = NULL;
-	node s;
-	int ch = 1;
-	while (ch)
-	{
-		switch (dispMenu())
-		{
-		case 1:
 
-			head =addNodeBeg(head,createNode());
-			break;
-		case 2:
-			head = addNodeEnd(head, createNode());
-			break;
-		case 3:
-			dispList(head);
-			break;
-		case 4:
-			ch = 0;
-			break;
-		default:
-			cout << "Something went wrong" << endl;
-		}
+class LList
+{
+private:
+	Node* n;
+public:
+	LList() {
+		n.data = 0;
+		n->next = nullptr;
 	}
-
-	cout << "Exiting program...." << endl;
-	return 0;
-}
-
-
-int dispMenu()
-{
-	int ch;
-	cout << "\tPress,\n\t1. Add Node Begining\n\t2. Add Node End\n";
-	cout << "\t3. Display List\n\t4. Exit..\n\tChoice: ";
-	cin >> ch;
-	return ch;
-}
-
-int dispList(node* head)
-{
-	cout << "\tList is " << endl;
-	cout << "\t";
-	while (head != NULL)
-	{
-		cout << head->data << " -> ";
-		head = head->next;
-	}
-	cout << "NULL" << endl;
-	return EXIT_SUCCESS;
-}
-
-node* addNodeEnd(node* head, node* nn)
-{
-	node* temp = head;
-	if (head == NULL) {
-		head = nn;
-		return head;
-	}
-	while (head->next != NULL)
-		head = head->next;
-	head->next = nn;
-	head = temp;
-	return head;
-}
-
-node* addNodeBeg(node* head, node* nn)
-{
-	if (head == NULL)
-	{
-		head = nn;
-		return head;
-	}
-	nn->next = head;
-	head = nn;
-	return head;
-}
-
-NODE* createNode()
-{
-	NODE* nn = (NODE*)malloc(sizeof(NODE));
-	cout << "Enter the value of node: ";
-	cin >> nn->data;
-	nn->next = NULL;
-	return nn;
-}
+};
